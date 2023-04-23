399d05bd3c87d1c5913c895167436f5c93c8a646
kemish bajgai <kemishbajgai4@gmail.com>
2023-04-23 13:00:38 +0545

    lots

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -1,28 +1,58 @@
 
 import { Link } from "react-router-dom"
+import "./Nab.css"
+import { GiHamburgerMenu } from "react-icons/gi"
+import { useState } from "react"
+
 
 const Heading = () => {
+  const[showMediaIcons, setShowMediaIcons] = useState(false);
   return (
-    <div>
-     <nav className="navbar navbar-expand-lg" style={{backgroundColor: "#CD853F"}}>
-  <div className="container-fluid">
-    <span className="navbar-brand $blue-200">Navbar</span>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link  className="nav-link active" aria-current="page" to="/" style={{color: "white"}}>Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/cart" style={{color: "white"}}>Carts</Link>
-        </li>
-      </ul>
+//     <div>
+//      <nav className="navbar navbar-expand-lg" style={{backgroundColor: "#CD853F"}}>
+//   <div className="container-fluid">
+//     <span className="navbar-brand $blue-200">Navbar</span>
+//     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+//       <span className="navbar-toggler-icon"></span>
+//     </button>
+//     <div className="collapse navbar-collapse" id="navbarNavDropdown">
+//       <ul className="navbar-nav">
+//         <li className="nav-item">
+//           <Link  className="nav-link active" aria-current="page" to="/" style={{color: "white"}}>Home</Link>
+//         </li>
+//         <li className="nav-item">
+//           <Link className="nav-link" to="/cart" style={{color: "white"}}>Carts</Link>
+//         </li>
+//       </ul>
+//     </div>
+//   </div>
+// </nav>
+//     </div>
+<div>
+  <nav className="main-nav">
+    <div className="logo">
+      <h2>
+        <span>N</span>avbar
+      </h2>
     </div>
-  </div>
-</nav>
+    <div className={showMediaIcons?  "menu-link mobile-menu-link": "menu-link"}>
+     <ul>
+      <li>
+      <Link className="a" to="/">Home</Link>
+       </li>
+       <li>
+       <Link className="a" to="/cart">Carts</Link>
+       </li>
+     </ul>
     </div>
+    <div className="hamburger-menu">
+    <Link to="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </Link>
+    </div>
+  </nav>
+ 
+</div>
   )
 }
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
     },[])
     return (
         <div className="container my-3">
-            <span className="my-3" style={{color: "#000080", marginLeft: "0px", fontFamily: "Lucida Console"}}>Hi, i am Kemish bajgain ☺️, Welcome to my shop. To order, add the items to the cart. Than we will contact you on the basis of your selection. These are the list of dummies of Products we have, Thank you for choosing us.</span>
+            <span className="my-3 my-3 my-3" style={{color: "#000080", marginLeft: "0px", fontFamily: "Lucida Console"}}>Hi, i am Kemish bajgain ☺️, Welcome to my shop. To order, add the items to the cart. Than we will contact you on the basis of your selection. These are the list of dummies of Products we have, Thank you for choosing us.</span>
           <div className="row my-3 my-3">
           {data?.map(prod => (
       <div className="col-md-4 mb-4" key={prod.id}>
